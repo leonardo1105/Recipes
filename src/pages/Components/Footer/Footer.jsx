327dc48dfb53fdbe5b9ae719f11f7c26cd371db8
Leonardo Nunes <leonardoportelanunes@gmail.com>
2022-09-29 16:13:05 -0400

    conteúdo adicionado no Sobre

---
+++ src/pages/Components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Facebook from '../../../image/footer/Facebook Ellipse.svg'
+
+export default function Footer(){
+  return(
+    <>
+    <div>
+      <img src={Facebook} alt="" />
+      <img src="" alt="" />
+      <img src="" alt="" />
+      <img src="" alt="" />
+    </div>
+    <ul>
+      <li>ABOUT</li>
+      <li>RECIPES</li>
+      <li>SUBSCRIBE</li>
+    </ul>
+    <div>
+      <p></p>
+    </div>
+    </>
+  )
+}
--- src/pages/Components/Main/mainAbout/StyleMainAbout.js
+++ src/pages/Components/Main/mainAbout/StyleMainAbout.js
@@ -9,6 +9,47 @@ export const Main = styled.section`
   background-color: #F2F4F1;
   display: flex;
   align-items: center;
-  flex-direction: column;
   padding-bottom: 10em;
+  background:white;
 `
+export const Colher=styled.img`
+  width:100%;
+  
+`
+
+export const CaixaImg=styled.figure`
+  width:50%;
+  height:100%;
+  margin-top:-1rem;
+`
+export const CaixaConteudo=styled.section`
+  width:50%;
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+
+  h2{
+    margin-top:-4.5rem;
+    margin-bottom:1rem;
+    font-family: 'Roboto';
+    color:#373737;
+    letter-spacing: 2px;
+    font-weight: 500;
+  }
+
+  p{
+    width:28.5rem;
+    line-height: 25px;
+    font-family: 'Roboto';
+    color:#373737;
+    font-weight:300;
+  }
+`
+
+export const Linha=styled.div`
+  width: 71px;
+  height: 3px;
+  background:#373737;
+  margin-bottom:4.5rem;
+`
+
